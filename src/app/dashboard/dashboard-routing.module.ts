e95968c537cf2514c5e9656e33075f934249986a
mjmanishjoshi <mj.manish.joshi@gmail.com>
2019-09-06 00:07:11 +0530

    Update dashboard-routing.module.ts

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
-const routes: Routes = [
+const routes: Routes = [ 
   { path: '', pathMatch: 'full', component: DashboardComponent }
 ];
 
